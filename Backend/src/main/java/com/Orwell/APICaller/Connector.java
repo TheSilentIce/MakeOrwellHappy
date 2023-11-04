0584f20045aad8c188493375390f7af92c0a6fa8
TheSilentIce <srikaryadlapati@gmail.com>
2023-11-04 17:32:57 -0600

    Finished JsonBuilder and Connector class, although Connector class may need some updating.

--- Backend/src/main/java/com/Orwell/APICaller/Connector.java
+++ Backend/src/main/java/com/Orwell/APICaller/Connector.java
@@ -1,6 +1,41 @@
 package com.Orwell.APICaller;
 
+import javax.net.ssl.HttpsURLConnection;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class Connector {
-    private final String model = "gpt-3.5-turbo";
+    private String connect(String json) throws IOException {
+        URL url = new URL("https://api.openai.com/v1/chat/completions");
+        HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setRequestMethod("POST");
+        APIKey apiKey = new APIKey();
+        String API_KEY = apiKey.getApi_key();
+        connection.setRequestProperty("Authorization", "Bearer " + API_KEY);
+        connection.setDoOutput(true);
+
+        try (OutputStream os = connection.getOutputStream()) {
+            byte[] input = json.getBytes("utf-8");
+            os.write(input, 0, input.length);
+        }
+
+        int code = connection.getResponseCode();
+        StringBuffer response = new StringBuffer();
+        if (code == HttpsURLConnection.HTTP_OK) {
+            InputStream inputStream = connection.getInputStream();
+            BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            connection.disconnect();
+        } else {
+            throw new RuntimeException("ERROR: " + code);
+        }
+        return response.toString();
+    }
 
 }
--- Backend/src/main/java/com/Orwell/APICaller/JsonBuilder.java
+++ Backend/src/main/java/com/Orwell/APICaller/JsonBuilder.java
@@ -1,4 +1,25 @@
 package com.Orwell.APICaller;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
 public class JsonBuilder {
+    private final String model = "gpt-3.5-turbo";
+    private final String prompt = "Generate a random but very creative 3 sentence passage. A user will add their own 3 sentences to it, so make sure one is able to add to it.";
+
+    public String build() {
+        Gson gson = new Gson();
+        JsonObject payload = new JsonObject();
+
+        payload.addProperty("model", model);
+        JsonObject[] messages = new JsonObject[1];
+
+        JsonObject userMessage = new JsonObject();
+        userMessage.addProperty("role", "user");
+        userMessage.addProperty("content", prompt);
+        messages[0] = userMessage;
+
+        payload.add("messages", gson.toJsonTree(messages));
+        return payload.toString();
+    }
 }
