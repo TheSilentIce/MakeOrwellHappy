519d3c0f31c58d23823da18c9a131a0aa477e6d7
TheSilentIce <srikaryadlapati@gmail.com>
2023-11-04 16:20:53 -0600

    Initial Setup

---
+++ Backend/src/main/java/com/Orwell/OrwellServer/OrwellServerApplication.java
@@ -0,0 +1,14 @@
+package com.Orwell.OrwellServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrwellServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrwellServerApplication.class, args);
+		//TEST
+	}
+
+}
---
+++ Backend/src/test/java/com/Orwell/OrwellServer/OrwellServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Orwell.OrwellServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrwellServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
