f2c8786bc7947962417128b20399af507ded8acc
TheSilentIce <srikaryadlapati@gmail.com>
2023-11-05 08:30:28 -0700

    Downloaded axios and finished getPrompt function

--- Frontend/Orwell-website/src/Components/MainBody.tsx
+++ Frontend/Orwell-website/src/Components/MainBody.tsx
@@ -1,5 +1,26 @@
 import "../Styles/MainBody.css";
+import axios from "axios";
+//First box will be text, second will be filler, third will be textbox
 
-function MainBody() {}
+function MainBody() {
+  const getPrompt = async () => {
+    const URL = "https://localhost:8080/v1/Orwell/getPrompt";
+
+    try {
+      const prompt = await axios.get(URL, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      return prompt;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const test = () => {};
+
+  return <></>;
+}
 
 export default MainBody;
