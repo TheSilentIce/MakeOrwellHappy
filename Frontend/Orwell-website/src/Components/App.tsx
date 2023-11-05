0f43545f29b9bcc9ce6624fb1a618d521e2e6f1f
TheSilentIce <srikaryadlapati@gmail.com>
2023-11-04 22:15:49 -0600

    Moving files into folders and setting up particles

---
+++ Frontend/Orwell-website/src/Components/App.tsx
@@ -0,0 +1,14 @@
+import Particle from "./Particle";
+import "../Styles/App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Particle></Particle>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ Frontend/Orwell-website/src/Components/Particle.tsx
@@ -0,0 +1,16 @@
+import "../Styles/Particle.css";
+
+function Particle() {
+  return (
+    <>
+      <div>
+        <div className="text">
+          <h2>Fire</h2>
+        </div>
+      </div>
+      {/* <div className="container"></div> */}
+    </>
+  );
+}
+
+export default Particle;
